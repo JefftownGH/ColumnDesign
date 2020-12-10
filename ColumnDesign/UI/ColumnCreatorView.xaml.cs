f40b1a522e7d049f4cde7931a430712b9e44fccd
Nice3point <nice3point@gmail.com>
2020-12-10 19:17:24 +0300

    Init values

--- ColumnDesign/UI/ColumnCreatorView.xaml.cs
+++ ColumnDesign/UI/ColumnCreatorView.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System.Windows;
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using ColumnDesign.Methods;
+using ColumnDesign.ViewModel;
 
 namespace ColumnDesign.UI
 {
@@ -15,13 +19,34 @@ namespace ColumnDesign.UI
         {
             _uiDoc = uiApp.ActiveUIDocument;
             _doc = _uiDoc.Document;
+            DataContext = new ColumnCreatorViewModel();
             InitializeComponent();
             _mExternalMethodWpfArg = eExternalMethodWpfArg;
+            InitializeFields();
+        }
+
+        private void InitializeFields()
+        {
+            Date.Text = DateTime.Today.ToString("dd/MM/yyyy");
         }
 
         private void ButtonDrawGates_OnClick(object sender, RoutedEventArgs e)
         {
             _mExternalMethodWpfArg.Raise(this);
+        }
+        private void WindowX_OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (WindowY.IsChecked == true)
+            {
+                WindowY.IsChecked = false;
+            }
+        }
+        private void WindowY_OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (WindowX.IsChecked == true)
+            {
+                WindowX.IsChecked = false;
+            }
         }
     }
 }
---
+++ ColumnDesign/ViewModel/ColumnCreatorViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace ColumnDesign.ViewModel
+{
+    public class ColumnCreatorViewModel
+    {
+        public int WidthX { get; set; }
+        public int LengthY { get; set; }
+        public int HeightZ { get; set; }
+    }
+
+    public class ValueRangeRule : ValidationRule
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var val = 0;
+            if (value != null && ((string) value).Length > 0)
+            {
+                if (!int.TryParse((string) value, out val))
+                {
+                    return new ValidationResult(false, "Illegal characters");
+                }
+            }
+            if (val < Min || val > Max)
+            {
+                return new ValidationResult(false, $"Please enter value in the range: {Min}-{Max}.");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
